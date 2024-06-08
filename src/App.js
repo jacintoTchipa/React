e1ae91177259d4f90e0228ac38dd8cf57dba2c5b
ribneves <lantunescampos@icloud.com>
2024-06-08 20:36:01 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Projeto from './pages/page'
+
+function App() {
+  return (
+    <div className="App">
+
+    <Projeto/>
+     
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/Components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+	return(
+			<div>
+				<p>Jacinto Tchipa</p>
+			</div>
+    )
+}
+
+
+export default Footer
